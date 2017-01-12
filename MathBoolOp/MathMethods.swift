bae122291d494992a654ddc87997ad876014f272
JimCampagno <jimcampagno@gmail.com>
2017-01-12 16:00:34 -0500

    Remove solution

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,42 +11,14 @@ import Foundation
 
 class MathMethods {
     
+    
+    
+    
+    
     // Implement your functions here.
     
     
-    // #1
-    func add(a: Double, b: Double) -> Double {
-        return a + b
-    }
     
-    // #2
-    func add(a: Int, b: Double) -> Int {
-        return a + Int(b)
-    }
     
-    // #3
-    func isEqual(a: Int, b: Int) -> Bool {
-        return a == b
-    }
-    
-    // #4
-    func isEqual(a: Double, b: Double) -> Bool {
-        return a == b
-    }
-    
-    // #5
-    func questionFive() -> String {
-        return "d"
-    }
-    
-    // #6
-    func compare(a: Int, b: Int) -> Bool {
-        return (a < b) || (a >= 100)
-    }
-    
-    // #7
-    func average(a: Int, b: Int, c: Int) -> Int {
-        return (a + b + c) / 3
-    }
 
 }
